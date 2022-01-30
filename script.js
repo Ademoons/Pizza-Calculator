60e1af3bde74190a8a0da3846fcb9ff2c9bb7f4d
G <71948411+Ademoons@users.noreply.github.com>
2022-01-30 13:50:17 +0100

    added another recipe, fixed minor layout issue

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 
 const recipe1Btn = document.getElementById("recipe1")
 const recipe2Btn = document.getElementById("recipe2")
+const recipe3Btn = document.getElementById("recipe3")
 const formArea = document.getElementById("form-container")
 const outputArea = document.getElementById("outputArea")
 
@@ -21,10 +22,15 @@ const oilRatio = 20
 
 const secondWaterRatio = 1.53846153846
 const secondYeastRatio = 400
-const secondSaltsecond = 38.4615384615
+const secondSaltRatio = 38.4615384615
+
+const thirdWaterRatio = 1.61812297735
+const thirdYeastRatio = 129.87012987
+const thirdSaltRatio = 43.4782608696
 
 recipe1Btn.addEventListener("click", generateFirst)
 recipe2Btn.addEventListener("click", generateSecond)
+recipe3Btn.addEventListener("click", generateThird)
 
 function renderFirst() {
     const form = document.getElementById("formElement")
@@ -49,7 +55,23 @@ function renderSecond() {
     let farina = form.farina.value
     acqua = getAcqua(farina, secondWaterRatio)
     lievito = getLievito(farina, secondYeastRatio)
-    sale = getSale(farina, secondSaltsecond)
+    sale = getSale(farina, secondSaltRatio)
+    if (farina > 0) {
+        outputArea.innerHTML = `
+            <p>Farina: ${farina}gr</p>
+            <p>Acqua: ${acqua}gr</p>
+            <p>Lievito: ${lievito}gr</p>
+            <p>Sale: ${sale}gr</p>
+            `
+        }            
+}
+
+function renderThird() {
+    const form = document.getElementById("formElement")
+    let farina = form.farina.value
+    acqua = getAcqua(farina, thirdWaterRatio)
+    lievito = getLievito(farina, thirdYeastRatio)
+    sale = getSale(farina, thirdSaltRatio)
     if (farina > 0) {
         outputArea.innerHTML = `
             <p>Farina: ${farina}gr</p>
@@ -62,6 +84,7 @@ function renderSecond() {
 
 function generateFirst() {
     formArea.innerHTML = `
+        <h2>Prima Ricetta</h2>
         <form id="formElement" onsubmit="return false">
         <input type="number" id="farina" placeholder="Farina">
         <button id="submit" type="submit">Genera</button>
@@ -74,6 +97,7 @@ function generateFirst() {
 
 function generateSecond() {
     formArea.innerHTML = `
+        <h2>Seconda Ricetta</h2>
         <form id="formElement" onsubmit="return false">
         <input type="number" id="farina" placeholder="Farina">
         <button id="submit" type="submit">Genera</button>
@@ -81,6 +105,18 @@ function generateSecond() {
     `
     let submitBtn = document.getElementById("submit")
     submitBtn.addEventListener("click", renderSecond)
+}
+
+function generateThird() {
+    formArea.innerHTML = `
+        <h2>Terza Ricetta</h2>
+        <form id="formElement" onsubmit="return false">
+        <input type="number" id="farina" placeholder="Farina">
+        <button id="submit" type="submit">Genera</button>
+        </form>
+    `
+    let submitBtn = document.getElementById("submit")
+    submitBtn.addEventListener("click", renderThird)    
 }
 
 function getAcqua(farina, ratio) {
